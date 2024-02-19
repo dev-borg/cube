236a50037bcf1154ea4d3770868b8fda6081014b
marco <marco.eu@aol.com>
2024-02-19 13:05:45 +0100

    3rd commit: git : starting to code in C

--- signals/code/c/main.c
+++ signals/code/c/main.c
@@ -1,1 +1,16 @@
-main.c
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(void)
+{
+	unsigned long int i = 1;
+	while(1)
+	{
+		printf("Looping, iteration #%021ld ...\n", i++);
+		(void) sleep(1);
+	}
+	exit (EXIT_SUCCESS);
+
+}
